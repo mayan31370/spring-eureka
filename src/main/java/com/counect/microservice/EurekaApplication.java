41c1cd9659ab320704f9136370902248a4092223
马岩 <yan.ma@counect.com>
2017-05-15 15:47:17 +0800

    集群测试

--- src/main/java/com/counect/microservice/EurekaApplication.java
+++ src/main/java/com/counect/microservice/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.counect.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -11,5 +12,6 @@ public class EurekaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EurekaApplication.class, args);
+    Environment
   }
 }
