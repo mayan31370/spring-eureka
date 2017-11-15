1cca1d34b7e3fbfd83a65076788f3ac3e7c79bc5
mayan <yan.ma@counect.com>
2017-11-15 16:42:46 +0800

    更名

--- src/main/java/mayan31370/microservice/EurekaApplication.java
+++ src/main/java/mayan31370/microservice/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.counect.microservice;
+package mayan31370.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
